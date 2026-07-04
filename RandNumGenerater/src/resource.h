1e3b5a7a32e68a437cc349def3331392e8af6ae8
Liu-PengHui <142143914+Liu-PengHui@users.noreply.github.com>
2026-07-04 19:58:20 +0800

    Add files via upload

---
+++ RandNumGenerater/src/resource.h
@@ -0,0 +1,53 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ 生成的包含文件。
+// 供 RandNumGtr.rc 使用
+//
+#define IDC_MYICON                      2
+#define IDD_RANDNUMGTR_DIALOG           102
+#define IDS_APP_TITLE                   103
+#define IDD_ABOUTBOX                    103
+#define IDM_ABOUT                       104
+#define IDM_EXIT                        105
+#define IDI_RANDNUMGTR                  107
+#define IDI_SMALL                       108
+#define IDC_RANDNUMGTR                  109
+#define IDC_EDIT1                       111
+#define IDC_EDIT2                       112
+#define IDC_BUTTON                      113
+#define IDC_OPENSRC                     114
+#define IDR_MAINFRAME                   128
+#define IDD_SETTINGS                    129
+#define IDM_SETTINGS                    200
+#define IDC_COLOR_SCHEME                1004
+#define IDC_RADIO_DEFAULT               1005
+#define IDC_RADIO_BLUE_DEFAULT          1005
+#define IDC_RADIO_DARK                  1006
+#define IDC_RADIO_PINK                  1006
+#define IDC_RADIO_BLUE                  1007
+#define IDC_RADIO_YELLOW                1007
+#define IDC_RADIO_GREEN                 1008
+#define IDC_RADIO_RED                   1008
+#define IDC_SLIDER1                     1016
+#define IDC_CHECK1                      1018
+#define IDC_CHECK_DARKMODE              1018
+#define ID_32774                        32774
+#define ID_32775                        32775
+#define ID_32776                        32776
+#define ID_32777                        32777
+#define IDM_UPDATELOG                   32778
+#define IDC_STATIC                      -1
+#define IDS_LOG_CONTENT                 104
+#define IDD_UPDATELOG                   132
+#define IDC_EDIT_LOG                    1022
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NO_MFC                     1
+#define _APS_NEXT_RESOURCE_VALUE        132
+#define _APS_NEXT_COMMAND_VALUE         32779
+#define _APS_NEXT_CONTROL_VALUE         1022
+#define _APS_NEXT_SYMED_VALUE           110
+#endif
+#endif
